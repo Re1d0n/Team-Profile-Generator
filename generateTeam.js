f6ded0e8c0caed92c66253643cb60201adb8d435
Re1d0n <reiduninnes@gmail.com>
2022-04-28 20:32:54 -0700

    added html

---
+++ generateTeam.js
@@ -0,0 +1,3 @@
+function generateTeam(Team){
+    
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const inquirer = require ('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
+const { type } = require('os');
 const Engineer=require ('./models/Engineer')
 const Intern=require ('./models/Intern')
 const Manager=require ('./models/Manager')
@@ -30,7 +32,91 @@ inquirer.prompt([
     console.log(
         Response
  )
- const officeManager
+ const officeManager= new Manager (Response.managerName,Response.managerId,Response.managerEmail,Response.managerOfficenumber)
 
+ Team.push(officeManager)
+teamBuild()
 }
 )
+
+function teamBuild(){
+    inquirer.prompt([
+        {
+            type:"list",
+            message:"Do you want to add members to your team?",
+            name:"teamAdd",
+            choices:["Engineer","Intern","Finish team"]
+        }
+    ]) .then((Response)=>{
+        if (Response.teamAdd==="Engineer"){
+  addEngineer()
+         } else if (Response.teamAdd==="Intern"){
+             addIntern()
+         } else {
+             createTeam()
+         }
+    })
+}
+function addEngineer(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Whats your engineers name?",
+            name: "engineerName"
+        },
+        {
+            type: "input",
+            message: "Whats your Engineers Id?",
+            name: "engineerId"
+        },
+        {
+            type: "input",
+            message: "Whats your Engineers email?",
+            name: "engineerEmail"
+        },
+        {
+            type:"input",
+            message:"Whats your engineers github username?",
+            name:"engineerGithub"
+        },
+        
+        ]) .then ((Response)=>{
+            const newEngineer = new Engineer (
+                Response.engineerName,Response.engineerId,Response.engineerEmail,Response.engineerGithub
+            )
+            Team.push(newEngineer)
+            teamBuild()
+        })
+} 
+ function addIntern(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your interns name?",
+            name: "internName"
+        },
+        {
+            type: "input",
+            message: "What is your interns id?",
+            name: "internId"
+        },
+        {
+            type: "input",
+            message: "What is your interns email?",
+            name: "internEmail"
+        },
+        {
+            type:"input",
+            message:"What is your interns school?",
+            name:"internSchool"
+        },
+        
+        ]) .then((Response)=>{
+            const newIntern= new Intern (Response.internName,Response.internId,Response.internEmail, Response.internSchool)
+            Team.push(newIntern)
+            teamBuild()
+        })
+ }
+ function createTeam(){
+console.log(Team)
+ }
